e4c9b0926d9c2fb86c580d4782cb5265f7e18d4a
Filipe de Souza <filipedesouza@MacBook-Pro-de-Filipe.local>
2015-05-18 10:19:19 -0300

    Initial Commit

---
+++ Custódia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Custódia
+//
+//  Created by Filipe de Souza on 18/05/15.
+//  Copyright (c) 2015 Filipe de Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Custódia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Custódia
+//
+//  Created by Filipe de Souza on 18/05/15.
+//  Copyright (c) 2015 Filipe de Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
